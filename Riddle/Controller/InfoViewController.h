acce65adb020b31cb5d2f7afa2756c7117a06c79
liroy y <liroy.yarimi@gmail.com>
2018-08-26 17:55:30 +0300

    complete info view controller

---
+++ Riddle/Controller/InfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.h
+//  Riddle
+//
+//  Created by liroy yarimi on 26.8.2018.
+//  Copyright © 2018 Liroy Yarimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+@end
---
+++ Riddle/Controller/InfoViewController.m
@@ -0,0 +1,37 @@
+//
+//  InfoViewController.m
+//  Riddle
+//
+//  Created by liroy yarimi on 26.8.2018.
+//  Copyright © 2018 Liroy Yarimi. All rights reserved.
+//
+
+#import "InfoViewController.h"
+
+@interface InfoViewController ()
+
+@end
+
+@implementation InfoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
