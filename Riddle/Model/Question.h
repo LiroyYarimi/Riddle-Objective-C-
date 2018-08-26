818285435bf4375c5ca827eda91790dc7e8bdd3d
liroy y <liroy.yarimi@gmail.com>
2018-08-26 13:18:31 +0300

    Add photos and create folders

---
+++ Riddle/Model/Question.h
@@ -0,0 +1,13 @@
+//
+//  Question.h
+//  Riddle
+//
+//  Created by liroy yarimi on 26.8.2018.
+//  Copyright © 2018 Liroy Yarimi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Question : NSObject
+
+@end
---
+++ Riddle/Model/Question.m
@@ -0,0 +1,13 @@
+//
+//  Question.m
+//  Riddle
+//
+//  Created by liroy yarimi on 26.8.2018.
+//  Copyright © 2018 Liroy Yarimi. All rights reserved.
+//
+
+#import "Question.h"
+
+@implementation Question
+
+@end
